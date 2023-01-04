ddd7098fa76c7f14b71f37586c38cd44fb8a018a
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-04 23:54:55 +0900

    layout

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,3 +1,11 @@
-<h1>
-  <a href="https://kit.svelte.dev"> 테스트 </a>
-</h1>
+<script>
+  let name = "wonjang";
+</script>
+
+<h1>{name}의 홈페이지임</h1>
+
+<style>
+  h1 {
+    font-size: 50px;
+  }
+</style>
